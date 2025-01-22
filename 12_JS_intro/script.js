c9eb5337118953b71ec771e47160da5344a0fee6
Mario Ebenhofer <manolol@mailbox.org>
2025-01-22 20:36:45 +0100

    12

--- 12_JS_intro/script.js
+++ 12_JS_intro/script.js
@@ -2,10 +2,16 @@ function calculate(callback) {
     let x = parseFloat(document.getElementById("num1").value);
     let y = parseFloat(document.getElementById("num2").value);
 
+    if (isNaN(x) || isNaN(y)) {
+        document.getElementById("result").innerHTML = "Please enter valid numbers!";
+        return;
+    }
+
     document.getElementById("result").innerHTML = callback(x, y);
 }
 
-document.getElementById("add").addEventListener("click", () => calculate(function(x, y) {return x + y}));
-document.getElementById("sub").addEventListener("click", () => calculate(function(x, y) {return x - y}));
-document.getElementById("mul").addEventListener("click", () => calculate(function(x, y) {return x * y}));
-document.getElementById("div").addEventListener("click", () => calculate(function(x, y) {return x / y}));
+document.getElementById("add").addEventListener("click", () => calculate((x, y) => x + y));
+document.getElementById("sub").addEventListener("click", () => calculate((x, y) => x - y));
+document.getElementById("mul").addEventListener("click", () => calculate((x, y) => x * y));
+document.getElementById("div").addEventListener("click", () => calculate((x, y) => x / y));
+document.getElementById("mod").addEventListener("click", () => calculate((x, y) => x % y));
