e11dd02de83429e7493ea6ec48a80d5d0a67d03b
Mario Ebenhofer <mario.ebenhofer@mailbox.org>
2025-03-24 15:03:45 +0100

    16

--- 16_JS_DOM/script.js
+++ 16_JS_DOM/script.js
@@ -1,46 +1,68 @@
 const myList = document.getElementById("myList");
 const input = document.getElementById("text-input");
 
+let entries = [{id: 0, name: "firstEntry", flag: '-'}, {id: 1, name: "secondEntry", flag: '-'}];
+
 init();
 
 function init() {
     document.getElementById("add-btn").addEventListener("click", handleOnAddClicked);
+
+    renderEntries();
+}
+
+function renderEntries() {
+    console.log(entries)
+    myList.innerHTML = "";
+
+    for (let entry of entries) {
+        addEntryToList(entry);
+    }
+}
+
+function addEntryToList(entry) {
+    const newElement = document.createElement("li");
+        const newText = document.createTextNode(entry);
+        newElement.appendChild(newText);
+
+        const deleteIcon = document.createElement("i");
+        deleteIcon.classList.add("fa-solid", "fa-circle-xmark", "action-button");
+        deleteIcon.addEventListener("click", (event) => {
+            for (let en of entries) {
+                if (en.id == entry.id) {
+                    
+                }
+            }
+        })
+        newElement.appendChild(deleteIcon);
+
+        const markIcon = document.createElement("i");
+        markIcon.classList.add("fa-solid", "fa-triangle-exclamation", "action-button");
+        markIcon.addEventListener("click", (event) => {
+            const parentNode = event.target.parentNode;
+            parentNode.classList.remove("done")
+            parentNode.classList.toggle("marked");
+        })
+        newElement.appendChild(markIcon);
+
+        const doneIcon = document.createElement("i");
+        doneIcon.classList.add("fa-solid", "fa-circle-check", "action-button");
+        doneIcon.addEventListener("click", (event) => {
+            const parentNode = event.target.parentNode;
+            parentNode.classList.remove("marked")
+            if (!parentNode.classList.contains("marked")) {
+                parentNode.parentNode.appendChild(parentNode);
+            }
+            parentNode.classList.toggle("done");
+        })
+        newElement.appendChild(doneIcon);
+
+        myList.appendChild(newElement);
 }
 
 function handleOnAddClicked() {
-    const newElement = document.createElement("li");
-    const newText = document.createTextNode(input.value);
-    newElement.appendChild(newText);
-
-    const deleteIcon = document.createElement("i");
-    deleteIcon.classList.add("fa-solid", "fa-circle-xmark", "action-button");
-    deleteIcon.addEventListener("click", (event) => {
-        event.target.parentNode.remove()
-    })
-    newElement.appendChild(deleteIcon);
-
-    const markIcon = document.createElement("i");
-    markIcon.classList.add("fa-solid", "fa-triangle-exclamation", "action-button");
-    markIcon.addEventListener("click", (event) => {
-        const parentNode = event.target.parentNode;
-        parentNode.classList.remove("done")
-        parentNode.classList.toggle("marked");
-    })
-    newElement.appendChild(markIcon);
-
-    const doneIcon = document.createElement("i");
-    doneIcon.classList.add("fa-solid", "fa-circle-check", "action-button");
-    doneIcon.addEventListener("click", (event) => {
-        const parentNode = event.target.parentNode;
-        parentNode.classList.remove("marked")
-        if (!parentNode.classList.contains("marked")) {
-            parentNode.parentNode.appendChild(parentNode);
-        }
-        parentNode.classList.toggle("done");
-    })
-    newElement.appendChild(doneIcon);
-
-    myList.appendChild(newElement);
-
+    if (input.value != "") entries.push(input.value);
     input.value = ""
+
+    renderEntries();
 }
